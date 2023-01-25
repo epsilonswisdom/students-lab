761f7ddbcb88ea4a89a35a6ac902afe760836975
Manny <emaldonado1402@gmail.com>
2023-01-25 17:38:38 -0500

    Fixing header

--- src/Score.jsx
+++ src/Score.jsx
@@ -2,7 +2,7 @@ const Score = (props) => {
   console.log(props, 'score')
   return (
     <div>
-      {props.studentName} recieved a score of: {props.score.score} on: {props.score.date}.
+      {props.studentName} scored: {props.score.score} on: {props.score.date}.
     </div>
   )
 }
